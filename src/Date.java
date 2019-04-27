b2704f81ef6219000aaeefb2df6560a415b99891
Thomas Faugier <thomas.faugier@gmail.com>
2019-04-27 17:39:32 +0200

    Update .gitignore and remove *.iml files

--- src/Date.java
+++ src/Date.java
@@ -3,8 +3,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Objects;
 
-final class Date implements Cloneable
-{
+final class Date implements Cloneable {
     final private LocalDate date;
     final private String display;
 
@@ -14,36 +13,30 @@ final class Date implements Cloneable
      *              Else, use Calendar.MONTH_NAME.
      * @param day   The day of birth. Starts with 1.
      */
-    Date(int year, int month, int day)
-    {
-        date = LocalDate.of(year, month + 1, day);
-        display = date.format(DateTimeFormatter.ofPattern("dd LLLL yyyy").withLocale(Locale.US));
+    Date(int year, int month, int day) {
+        date = LocalDate.of( year, month + 1, day );
+        display = date.format( DateTimeFormatter.ofPattern( "dd LLLL yyyy" ).withLocale( Locale.US ) );
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         return display;
     }
 
     @Override
-    public boolean equals(Object o)
-    {
-        if (this == o)
-        {
+    public boolean equals(Object o) {
+        if (this == o) {
             return true;
         }
-        if (o == null || getClass() != o.getClass())
-        {
+        if (o == null || getClass() != o.getClass()) {
             return false;
         }
         Date date1 = (Date) o;
-        return date.equals(date1.date);
+        return date.equals( date1.date );
     }
 
     @Override
-    public int hashCode()
-    {
-        return Objects.hash(date);
+    public int hashCode() {
+        return Objects.hash( date );
     }
 }
--- src/Human.java
+++ src/Human.java
@@ -1,46 +1,37 @@
 import java.util.Objects;
 
-public abstract class Human implements Cloneable
-{
+public abstract class Human implements Cloneable {
     protected String firstName;
     protected String lastName;
 
-    public String getLastName()
-    {
+    public String getLastName() {
         return lastName;
     }
 
-    public String getFirstName()
-    {
+    public String getFirstName() {
         return firstName;
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         return "(" + firstName + ", " + lastName + ")";
     }
 
     @Override
-    public boolean equals(Object o)
-    {
-        if (this == o)
-        {
+    public boolean equals(Object o) {
+        if (this == o) {
             return true;
         }
-        if (o == null || getClass() != o.getClass())
-        {
+        if (o == null || getClass() != o.getClass()) {
             return false;
         }
         Human human = (Human) o;
-        return Objects.equals(firstName, human.firstName) &&
-            Objects.equals(lastName, human.lastName);
+        return Objects.equals( firstName, human.firstName ) && Objects.equals( lastName, human.lastName );
     }
 
     @Override
-    public int hashCode()
-    {
-        return Objects.hash(firstName, lastName);
+    public int hashCode() {
+        return Objects.hash( firstName, lastName );
     }
 
     @Override
